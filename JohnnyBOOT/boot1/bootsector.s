f099ab13933c370751e3787c6d0423b0f6c7f228
Johnny Sun <bmy001@gmail.com>
2014-01-17 11:27:18 +0800

    Move JohnnyBOOT to Saturn V

--- JohnnyBOOT/boot1/bootsector.s
+++ JohnnyBOOT/boot1/bootsector.s
@@ -18,15 +18,20 @@ go:  mov  ax,cs   ! 段寄存器cs值-->ax，用于初始化数据段寄存器ds
 mov  ds,ax
 mov  es,ax
 mov  [msg1+17],ah   ! 0x07-->替换字符串中1个点符号，喇叭将会鸣一声。
-mov  cx,#20   ! 共显示20个字符，包括回车换行符。
+mov  cx,#40   ! 共显示20个字符，包括回车换行符。
 mov  dx,#0x1004 ! 字符串将显示在屏幕第17行、第5列处。
 mov  bx,#0x000c ! 字符显示属性（红色）。
 mov  bp,#msg1 ! 指向要显示的字符串（中断调用要求）。
 mov  ax,#0x1301 ! 写字符串并移动光标到串结尾处。
 int  0x10 ! BIOS中断调用0x10，功能0x13，子功能01。
-loop0: jmp  loop0   ! 死循环。
-msg1:.ascii "Loading system ..."! 调用BIOS中断显示的信息。共20个ASCII码字符。
-.byte 13,10 
+loop0: 
+hlt
+jmp  loop0   ! 死循环。
+msg1:
+.ascii "Loading system ..."! 调用BIOS中断显示的信息。共20个ASCII码字符。
+.byte 13, 10 
+.ascii "Hello Kernel World!"
+.byte 13, 10
 .org 510  ! 表示以后语句从地址510(0x1FE)开始存放。
 .word 0xAA55   ! 有效引导扇区标志，供BIOS加载引导扇区使用。
 .text
