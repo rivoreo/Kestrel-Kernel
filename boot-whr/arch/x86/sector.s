f21734ba54efa1904b2331bd7cc0d06447e489b0
WHR <msl0000023508@gmail.com>
2014-01-17 13:15:21 +0800

    Change dir to arch/x86

--- boot-whr/arch/x86/sector.s
+++ boot-whr/arch/x86/sector.s
@@ -1,9 +1,5 @@
-;
-; 用代码0x07替换串msg1中1字符，然后在屏幕第1行上显示。
-;
-
-.globl begtext, begdata, begbss, endtext, enddata, endbss
-.text		; 正文段。
+	.globl begtext, begdata, begbss, endtext, enddata, endbss
+	.text		; 正文段。
 
 begtext:
 	.data	; 数据段。
@@ -15,18 +11,17 @@ begbss:
 	.text	; 正文段。
 
 BOOTSEG = 0x07c0	; BIOS加载bootsect代码的原始段地址。
-
 entry start	; 告知链接程序，程序从start标号处开始执行。
 
 start:
-	jmpi	go,BOOTSEG	; 段间跳转。BOOTSEG指出跳转段地址，标号go是偏移地址。
+	jmpi	go, BOOTSEG	; 段间跳转。BOOTSEG指出跳转段地址，标号go是偏移地址。
 
 go:
 	mov	ax, cs		; 段寄存器cs值-->ax，用于初始化数据段寄存器ds和es。
 	mov	ds, ax
 	mov	es, ax
 	;mov	[msg1+17], ah	; 0x07-->替换字符串中1个点符号，喇叭将会鸣一声。
-	mov	cx, #11  	; size of string
+	mov	cx, #22  	; size of string
 	;mov	dx, #0x1004	; 字符串将显示在屏幕第17行、第5列处。
 	mov	bx, #0x000c	; 字符显示属性（红色）。
 	mov	bp, #msg1	; 指向要显示的字符串（中断调用要求）。
@@ -38,7 +33,7 @@ loop0:
 	jmp	loop0		; 死循环。
 
 msg1:
-	.ascii	"Loading...\n"	; 调用BIOS中断显示的信息。共20个ASCII码字符。
+	.ascii	"Starting Kestrel...\r\n"
 	.byte	13, 10
 	.org	510		; 表示以后语句从地址510(0x1FE)开始存放。
 	.word	0xAA55		; 有效引导扇区标志，供BIOS加载引导扇区使用。
