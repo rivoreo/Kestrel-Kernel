1ce7d9ff833bfa8d10b3e0c5f894e96d0e20b6e6
WHR <msl0000023508@gmail.com>
2014-01-17 18:04:42 +0800

    update-mbr command

--- boot-whr/arch/x86/sector.s
+++ boot-whr/arch/x86/sector.s
@@ -1,4 +1,5 @@
 	.globl begtext, begdata, begbss, endtext, enddata, endbss
+
 	.text		; 正文段。
 
 begtext:
@@ -14,10 +15,10 @@ BOOTSEG = 0x07c0	; BIOS加载bootsect代码的原始段地址。
 entry start	; 告知链接程序，程序从start标号处开始执行。
 
 start:
-	jmpi	go, BOOTSEG	; 段间跳转。BOOTSEG指出跳转段地址，标号go是偏移地址。
+	jmpi	go, BOOTSEG	; 段间跳转。BOOTSEG指出跳转段地址，标号go是偏移地址
 
 go:
-	mov	ax, cs		; 段寄存器cs值-->ax，用于初始化数据段寄存器ds和es。
+	mov	ax, cs		; 段寄存器cs值-->ax，用于初始化数据段寄存器ds和es
 	mov	ds, ax
 	mov	es, ax
 	;mov	[msg1+17], ah	; 0x07-->替换字符串中1个点符号，喇叭将会鸣一声。
@@ -25,8 +26,9 @@ go:
 	;mov	dx, #0x1004	; 字符串将显示在屏幕第17行、第5列处。
 	mov	bx, #0x000c	; 字符显示属性（红色）。
 	mov	bp, #msg1	; 指向要显示的字符串（中断调用要求）。
-	mov	ax, #0x1301	; 写字符串并移动光标到串结尾处。
-	int	0x10		; BIOS中断调用0x10，功能0x13，子功能01。
+	mov	ax, #0x1301	; 功能0x13，子功能01（写字符串并移动光标到串结尾处）
+	int	0x10		; BIOS中断调用0x10
+	;jmp	go
 
 loop0:
 	hlt
