ebf1d4d4f209fe8b49759c4c9c63c9c03f7b664c
WHR <msl0000023508@gmail.com>
2014-01-29 00:58:06 +0800

    FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!! 	 FUCK GIT!!!!!!!!!!

---
+++ Saturn-V/S-II/shell/commands/malloctest.c
@@ -0,0 +1,33 @@
+/*	A part of kestrel
+	Kestrel Kernel Project
+	Copyright 2007-2014 BMY-Soft
+	Copyright 2007-2014 PC GO Ld.
+
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#include <kestrel/kernel.h>
+
+int malloctest_command(int argc, char **argv) {
+	if(argc < 2) return 1;
+	unsigned int count = atoi(argv[1]);
+	size_t alloc_size = 8;
+	if(!count) {
+		puts("\"count = 0\" is invalid");
+		return 1;
+	}
+	if(argc > 2) alloc_size = atoi(argv[2]);
+	void **l = malloc(count * sizeof(void *));
+	printf("l = 0x%lx\n", (long int)l);
+	int i;
+	for(i=0; i<count; i++) {
+		l[i] = malloc(alloc_size);
+		printf("l[%u] = 0x%lx\n", i, l[i]);
+	}
+	puts("Now, free thems");
+	for(i=0; i<count; i++) free(l[i]);
+	free(l);
+	return 0;
+}
