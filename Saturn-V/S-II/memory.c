2253739023e177914152761ff95cad7a30aa7f03
JohnnySun <bmy001@gmail.com>
2014-10-14 15:28:28 +0800

    memory.c bug fixed

--- Saturn-V/S-II/memory.c
+++ Saturn-V/S-II/memory.c
@@ -6,7 +6,7 @@
 	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
 	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-*/
+	*/
 
 #include <kestrel/kernel.h>
 #include <kestrel/errno.h>
@@ -69,11 +69,11 @@ void init_memory() {
 }
 
 /*
-void kernel_malloc_manage(void *a, size_t len) {
-	//kernel_memset(a, 0, len);
-	char *p = a;
-	while(len--) *p++ = 0;
-	managed_address_end = (char *)a + len;
+   void kernel_malloc_manage(void *a, size_t len) {
+//kernel_memset(a, 0, len);
+char *p = a;
+while(len--) *p++ = 0;
+managed_address_end = (char *)a + len;
 }*/
 
 void kernel_malloc_init(void *start) {
@@ -104,112 +104,106 @@ void *kernel_malloc(size_t numbytes) {
 	while(current_location != last_address) {
 		//kernel_printf("current_location = 0x%x\nlast_address = 0x%x\n", current_location, last_address);
 		current_location_mcb = (struct mem_control_block *)current_location;
-<<<<<<< HEAD
 		if(current_location_mcb->is_available) { 
 			//如果此块内存空间大小正好等于申请大小 
-=======
-		//if(current_location_mcb->is_available || !current_location_mcb->size) {
-		if(current_location_mcb->is_available) {
->>>>>>> 7158726976057036f796a05d0753fc40f41744ec
-			if(current_location_mcb->size == numbytes) {
-				// 如果此块内存空间大小正好等于申请大小
-				current_location_mcb->is_available = 0;
-				memory_location = current_location;
-				current_location = (char *)current_location + current_location_mcb->size;
-				break;
-			} else if(current_location_mcb->size < numbytes) {
-				// 如果大小不够，尝试进行内存整理
-				tmp_location = (char *)current_location + current_location_mcb->size;
-				tmp_location_mcb = (struct mem_control_block*)tmp_location;
-				// 判断此mcb是否为内存断址
-				if(tmp_location >= last_address) {
-					//放弃此段内存空间，向下寻找合适内存空间
+			//if(current_location_mcb->is_available || !current_location_mcb->size) {
+			if(current_location_mcb->is_available) {
+				if(current_location_mcb->size == numbytes) {
+					// 如果此块内存空间大小正好等于申请大小
+					current_location_mcb->is_available = 0;
+					memory_location = current_location;
 					current_location = (char *)current_location + current_location_mcb->size;
-					continue;
-					//break;
-					//如果下一段内存空间被占用，放弃使用此块内存
-				} else if(!tmp_location_mcb->is_available) {
-					//放弃此段内存空间，向下寻找合适内存空间
-					current_location = (char *)current_location + current_location_mcb->size;
-					continue;
-					//break;
+					break;
+				} else if(current_location_mcb->size < numbytes) {
+					// 如果大小不够，尝试进行内存整理
+					tmp_location = (char *)current_location + current_location_mcb->size;
+					tmp_location_mcb = (struct mem_control_block*)tmp_location;
+					// 判断此mcb是否为内存断址
+					if(tmp_location >= last_address) {
+						//放弃此段内存空间，向下寻找合适内存空间
+						current_location = (char *)current_location + current_location_mcb->size;
+						continue;
+						//break;
+						//如果下一段内存空间被占用，放弃使用此块内存
+					} else if(!tmp_location_mcb->is_available) {
+						//放弃此段内存空间，向下寻找合适内存空间
+						current_location = (char *)current_location + current_location_mcb->size;
+						continue;
+						//break;
+					} else {
+						//合并相邻内存空间，使其成为一块大的内存空间
+						current_location_mcb->size += tmp_location_mcb->size;
+						continue;
+					}
 				} else {
-					//合并相邻内存空间，使其成为一块大的内存空间
-					current_location_mcb->size += tmp_location_mcb->size;
-					continue;
+					//如果内存空间大于申请空间，对此区域空间进行分割
+					current_location_mcb->is_available = 0;
+					//分割内存空间，重建mcb
+					tmp_location_mcb = (struct mem_control_block*)current_location + numbytes;
+					tmp_location_mcb->size = current_location_mcb->size - numbytes;
+					tmp_location_mcb->is_available = 1;
+					current_location_mcb->size = numbytes;
+					//----------内存空间分割完成----------//
+					tmp_location = (char *)current_location + current_location_mcb->size;
+					tmp_location_mcb = (struct mem_control_block*)tmp_location;
+					memory_location = current_location;
+					current_location = (char *)current_location + current_location_mcb->size;
+					break;
 				}
-			} else {
-				//如果内存空间大于申请空间，对此区域空间进行分割
-				current_location_mcb->is_available = 0;
-				//分割内存空间，重建mcb
-				tmp_location_mcb = (struct mem_control_block*)current_location + numbytes;
-				tmp_location_mcb->size = current_location_mcb->size - numbytes;
-				tmp_location_mcb->is_available = 1;
-				current_location_mcb->size = numbytes;
-				//----------内存空间分割完成----------//
-				tmp_location = (char *)current_location + current_location_mcb->size;
-				tmp_location_mcb = (struct mem_control_block*)tmp_location;
+
+			} else if(!current_location_mcb->size) {
 				memory_location = current_location;
-				current_location = (char *)current_location + current_location_mcb->size;
 				break;
+			} else if(current_location_mcb->size > 65536) {
+				panic("kernel_malloc: current_location_mcb->size too large");
 			}
-
-		} else if(!current_location_mcb->size) {
-			memory_location = current_location;
-			break;
-		} else if(current_location_mcb->size > 65536) {
-			panic("kernel_malloc: current_location_mcb->size too large");
+			//放弃此段内存空间，向下寻找合适内存空间
+				//在这放置下面的代码会导致alloc无限循环
+				//current_location = (char *)current_location + current_location_mcb->size;
+				size_t current_size = current_location_mcb->size;
+			kernel_printf("current_size = %u\n", current_size);
+			if(!current_size) panic("kernel_malloc: MCB Size Exception");
+			current_location = (char *)current_location + current_size;
+			/*
+			   if(current_location >= managed_address_end) {
+			   if((char *)current_location >= (char *)managed_address_end + STEP_SIZE) {
+			   panic("kernel_malloc: current_location out of STEP_SIZE");
+			   }
+			   kernel_malloc_manage(managed_address_end, STEP_SIZE);
+			   }*/
 		}
-		//放弃此段内存空间，向下寻找合适内存空间
-<<<<<<< HEAD
-		//在这放置下面的代码会导致alloc无限循环
-		//current_location = (char *)current_location + current_location_mcb->size;
-=======
-		size_t current_size = current_location_mcb->size;
-		kernel_printf("current_size = %u\n", current_size);
-		if(!current_size) panic("kernel_malloc: MCB Size Exception");
-		current_location = (char *)current_location + current_size;
-		/*
-		if(current_location >= managed_address_end) {
-			if((char *)current_location >= (char *)managed_address_end + STEP_SIZE) {
-				panic("kernel_malloc: current_location out of STEP_SIZE");
-			}
-			kernel_malloc_manage(managed_address_end, STEP_SIZE);
-		}*/
->>>>>>> 7158726976057036f796a05d0753fc40f41744ec
+
+		if(!memory_location) {
+			memory_location = last_address;		//取得地址
+			last_address = (char *)last_address + numbytes;
+			current_location_mcb = (struct mem_control_block *)memory_location;		//准备写入内存控制块(MCB)
+			current_location_mcb->is_available = 0;		//写入控制块
+			current_location_mcb->size = numbytes;
+		}
+		//将指针偏移移动过控制块(MCB)
+		memory_location = (char *)memory_location + sizeof(struct mem_control_block);
+
+		errno = 0;
+
+		//返回指针
+		return memory_location;
+		}
 	}
-
-	if(!memory_location) {
-		memory_location = last_address;		//取得地址
-		last_address = (char *)last_address + numbytes;
-		current_location_mcb = (struct mem_control_block *)memory_location;		//准备写入内存控制块(MCB)
-		current_location_mcb->is_available = 0;		//写入控制块
-		current_location_mcb->size = numbytes;
+	void kernel_free(void *firstbyte) {
+		if(!firstbyte) return;
+		if(!allocater_is_inited) panic("free: allocater has not inited");
+		struct mem_control_block *mcb;
+		//回退到控制块
+		mcb = (struct mem_control_block *)((char *)firstbyte - sizeof(struct mem_control_block));
+		errno = 0;
+		//判断地址合法性
+		if(mcb->is_available) {
+			kernel_printf("free: In address 0x%lx\n", (long int)firstbyte);
+			errno = EFAULT;
+			if(mcb->is_available > 1) panic("free: Invalid address!");
+			kernel_puts("Warning: Double free!!!");
+			errno = EFAULT;
+		}
+		//Woo!! 成功释放内存，啦啦啦~~~
+		mcb->is_available = 1;
 	}
-	//将指针偏移移动过控制块(MCB)
-	memory_location = (char *)memory_location + sizeof(struct mem_control_block);
-
-	errno = 0;
-
-	//返回指针
-	return memory_location;
-}
-
-void kernel_free(void *firstbyte) {
-	if(!firstbyte) return;
-	if(!allocater_is_inited) panic("free: allocater has not inited");
-	struct mem_control_block *mcb;
-	//回退到控制块
-	mcb = (struct mem_control_block *)((char *)firstbyte - sizeof(struct mem_control_block));
-	errno = 0;
-	//判断地址合法性
-	if(mcb->is_available) {
-		kernel_printf("free: In address 0x%lx\n", (long int)firstbyte);
-		errno = EFAULT;
-		if(mcb->is_available > 1) panic("free: Invalid address!");
-		kernel_puts("Warning: Double free!!!");
-		errno = EFAULT;
-	}
-	//Woo!! 成功释放内存，啦啦啦~~~
-	mcb->is_available = 1;
-}
